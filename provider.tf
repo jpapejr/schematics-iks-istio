6727c1e6c1b42a88b71938327152276d641cb275
John Pape <jpapejr@icloud.com>
2019-08-27 17:39:07 -0400

    update provider keys

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "ibm" {
-  bluemix_api_key = "${var.ibm_cloud_api_key}"
+  ibmcloud_api_key = "${var.ibm_cloud_api_key}"
   softlayer_username = "${var.sl_username}"
   softlayer_api_key = "${var.sl_api_key}"
 }
